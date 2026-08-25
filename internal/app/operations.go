05d09451aa3e935f524802ce15b73cc34173fb4a
lacsar712 <lacsar712@users.noreply.github.com>
2026-08-25 10:48:12 +0800

    init: slagquen

---
+++ internal/app/operations.go
@@ -0,0 +1,30 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/lacsar712/slagquen/internal/model"
+)
+
+func (a *App) ValidateSlagDrift(ctx context.Context, moistPct float64) error {
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+	limit := a.cfg.TargetMoistPct + a.cfg.MaxGradientDeltaPct
+	if moistPct <= limit {
+		return nil
+	}
+	return fmt.Errorf("moisture: %v", model.ErrSlagDrift)
+}
+
+func (a *App) ConfirmGranHold(ctx context.Context, anchor time.Time) error {
+	if a.avgWindow == nil {
+		return model.Wrap("app", "window", model.ErrGranHold)
+	}
+	if err := a.avgWindow.Require(anchor); err != nil {
+		return fmt.Errorf("gradient hold: %w", err)
+	}
+	return nil
+}
